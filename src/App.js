ae3ec74deeb74c51d2223e7d630c3ffd3e1e40c9
Jasmine-21 <jasmine171353.cse@chitkara.edu.in>
2020-05-11 11:37:24 +0530

    List of Menu

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import {Navbar, NavbarBrand} from 'reactstrap';
-import Menu from './components/MenuComponent';
+
+import Menu from './components/MenuCompnent';
 import './App.css';
 
 class App extends Component{
@@ -14,7 +15,7 @@ class App extends Component{
          <NavbarBrand href="/">Restaurant</NavbarBrand>
        </div>
      </Navbar>
-     <Menu/>
+     <Menu />
     </div>
   );
 }
